2636188fe83bfa4625592d319987a2a11150c44c
Devin Lewis <dglewis@umich.edu>
2023-01-25 21:31:10 -0500

    base

---
+++ src/test/java/com/wolfpack/User/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfpack.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
